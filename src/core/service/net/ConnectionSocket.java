82adbbfe44ad9a914355010ce5c68f83479c2bc5
Matheus Tenorio <tenoriomatheus0@gmail.com>
2016-02-28 11:06:10 -0300

    Update ConnectionSocket class

--- src/core/service/net/ConnectionSocket.java
+++ src/core/service/net/ConnectionSocket.java
@@ -5,24 +5,19 @@ import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.*;
-import java.util.LinkedList;
-import java.util.Queue;
+import java.util.Observable;
 
-public class ConnectionSocket {
+public class ConnectionSocket extends Observable implements Runnable {
 	private Socket _socket;
 	private boolean _isConnected;
-	
-	private ListenerFromServer _listener;
 	private BufferedReader _inputFromServer;
-	private Queue<String> _messagesFromServer;
-	
 	private DataOutputStream _outputToServer;
 	
 	public final static int SERVER_PORT = 95;
 	public final static String SERVER_IP = "";
 	
 	public ConnectionSocket() {
-		_messagesFromServer = new LinkedList<String>();
+		
 	}
 	
 	public void connect() {
@@ -30,9 +25,6 @@ public class ConnectionSocket {
 			if (!_isConnected) {
 				_socket = new Socket(SERVER_IP, SERVER_PORT);
 				_inputFromServer = new BufferedReader(new InputStreamReader(_socket.getInputStream()));
-				_listener = new ListenerFromServer(_inputFromServer, this);
-
-				_listener.start();
 				_isConnected = true;
 			}
 		} catch (IOException e) {
@@ -64,8 +56,10 @@ public class ConnectionSocket {
 		}
 	}
 	
-	public String read() {
-		return _messagesFromServer.poll();
+	public Thread startListen() {
+		Thread out = new Thread(this);
+		out.start();
+		return out;
 	}
 	
 	public boolean isConnected()
@@ -73,7 +67,19 @@ public class ConnectionSocket {
 		return _isConnected;
 	}
 
-	public void addMessageFromServer(String serverMsg) {
-		_messagesFromServer.add(serverMsg);
+	@Override
+	public void run() {
+		while (true)
+		{
+			String _serverMsg = null;
+			
+			try {
+				_serverMsg = _inputFromServer.readLine();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			
+			this.notifyObservers(new String(_serverMsg));
+		}
 	}
 }
