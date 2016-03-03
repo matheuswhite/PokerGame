c82f592ef8c791c0967f0bb0d67b695ad5c1c71f
Matheus Tenorio <tenoriomatheus0@gmail.com>
2016-03-03 12:09:41 -0300

    Fix client socket class

--- src/core/service/net/ClientSocket.java
+++ src/core/service/net/ClientSocket.java
@@ -5,77 +5,86 @@ import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.*;
-import java.util.Observable;
+import java.util.LinkedList;
+import java.util.Queue;
 
-public class ClientSocket extends Observable implements Runnable {
+public class ClientSocket implements Runnable {
 	private Socket _socket;
 	private boolean _isConnected;
+	private Queue<String> _serverMessages;
 	private BufferedReader _inputFromServer;
 	private DataOutputStream _outputToServer;
 	
 	public final static int SERVER_PORT = 95;
 	public final static String SERVER_IP = "";
 	
-	public void connect() {
-		try {
-			if (!_isConnected) {
-				_socket = new Socket(SERVER_IP, SERVER_PORT);
-				_inputFromServer = new BufferedReader(new InputStreamReader(_socket.getInputStream()));
-				_isConnected = true;
-			}
-		} catch (IOException e) {
-			e.printStackTrace();
-		} catch (NullPointerException e) {
-			e.printStackTrace();
+	public ClientSocket() {
+		_serverMessages = new LinkedList<String>();
+	}
+	
+	public void connect() throws UnknownHostException, IOException {
+		if (!_isConnected) {
+			_socket = new Socket(SERVER_IP, SERVER_PORT);
+			_inputFromServer = new BufferedReader(new InputStreamReader(_socket.getInputStream()));
+			_outputToServer = new DataOutputStream(_socket.getOutputStream());
+			
+			new Thread(this).start();
+			_isConnected = true;
 		}
 	}
 	
-	public void disconnect() {
-		try {
-			if (_isConnected) {
-				_socket.close();
-				_isConnected = false;
-			}
-		} catch (IOException e) {
-			e.printStackTrace();
+	public void disconnect() throws IOException {
+		if (_isConnected) {
+			_socket.close();
+			_isConnected = false;
 		}
 	}
 	
-	public void write(String message) {
-		try {
-			if (_isConnected) {
-				_outputToServer = new DataOutputStream(_socket.getOutputStream());
-				_outputToServer.writeBytes(message);
-			}
-		} catch (IOException e) {
-			e.printStackTrace();
+	public void write(String message) throws IOException {
+		if (_isConnected) {
+			_outputToServer.writeBytes(message);
 		}
 	}
 	
-	public Thread startListen() {
-		Thread out = new Thread(this);
-		out.start();
-		return out;
+	public Queue<String> readAll() {
+		if (!_serverMessages.isEmpty()) {
+			Queue<String> out = _serverMessages;
+			_serverMessages.clear();
+			return out;
+		}
+		return null;
+	}
+	
+	public String read() {
+		if (!_serverMessages.isEmpty()) {
+			return _serverMessages.poll();
+		}
+		
+		return null;
 	}
 	
 	public boolean isConnected()
 	{
 		return _isConnected;
 	}
-
+	
 	@Override
 	public void run() {
-		while (true)
-		{
-			String _serverMsg = null;
+		while (true) {
+			String checker = null;
+			String serverMessage = null;
 			
-			try {
-				_serverMsg = _inputFromServer.readLine();
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
+			do {
+				try {
+					checker = _inputFromServer.readLine();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+				if (checker != null) 
+					serverMessage += checker;
+			} while (checker != null);
 			
-			this.notifyObservers(new String(_serverMsg));
+			_serverMessages.add(serverMessage);
 		}
 	}
 }
