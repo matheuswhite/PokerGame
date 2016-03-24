42bfa4cb4008be5caedccec72bc3d91290fb1969
Matheus Tenorio <tenoriomatheus0@gmail.com>
2016-03-24 14:04:43 -0300

    Add a connection test screen

--- src/core/Launcher.java
+++ src/core/Launcher.java
@@ -1,34 +1,10 @@
 package core;
 
-import java.awt.Color;
-import java.awt.Point;
-import java.awt.Rectangle;
-
-import core.ui.graphic.Image;
-import core.ui.graphic.Label;
-import core.ui.graphic.TextStyle;
-import core.ui.graphic.Window;
-import core.ui.input.Button;
-import core.ui.input.NumberBox;
-import core.ui.input.TextBox;
+import core.ui.graphic.screen.ConnectionTestScreen;
 
 public class Launcher {
 	
 	public static void main(String[] args) {
-		Window w = new Window(700, 500, "PokerGame");
-		Label l = new Label(new Point(50, 50), "Text Label!", new TextStyle(Color.BLACK, "Arial", 12, false, false));
-		Button b = new Button(new Rectangle(250, 250, 70, 30), "Go!", Color.RED, Color.WHITE, null);
-		TextBox tb = new TextBox(new Rectangle(200, 50, 100, 25));
-		NumberBox nb = new NumberBox(new Rectangle(300, 400, 50, 25), 0, 200);
-		Image i = new Image(new Rectangle(450, 150, 1280, 551), "src/imgs/cards.jpeg");
-		
-		i.crop(new Rectangle(0, 0, 99, 138));
-		//i.resize(0.6, false);
-		
-		w.addComponent(l);
-		w.addComponent(b);
-		w.addComponent(tb);
-		w.addComponent(nb);
-		w.addComponent(i);
+		ConnectionTestScreen screen = new ConnectionTestScreen();
 	}
 }
---
+++ src/core/ui/graphic/screen/ConnectionTestScreen.java
@@ -0,0 +1,76 @@
+package core.ui.graphic.screen;
+
+import java.awt.Color;
+import java.awt.Point;
+import java.awt.Rectangle;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+import core.service.net.ServerConnection;
+import core.ui.graphic.Label;
+import core.ui.graphic.TextStyle;
+import core.ui.graphic.Window;
+import core.ui.input.Button;
+
+public class ConnectionTestScreen extends Window {
+
+	private Thread _connectionThread;
+	private ServerConnection _connection;
+	private Label _messageFromServer;
+	private Label _lbl1;
+	private Map<String, Button> _buttons;
+	
+	public ConnectionTestScreen() {
+		super(400, 500, "Connection tests");
+		
+		_connection = new ServerConnection();
+		initializeComponents();
+		try {
+			_connection.connect();
+			_connectionThread = new Thread(_connection);
+			_connectionThread.start();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	/*
+	 * Label l = new Label(new Point(50, 50), "Text Label!", new TextStyle(Color.BLACK, "Arial", 12, false, false));
+		Button b = new Button(new Rectangle(250, 250, 70, 30), "Go!", Color.RED, Color.WHITE, null);
+		TextBox tb = new TextBox(new Rectangle(200, 50, 100, 25));
+		NumberBox nb = new NumberBox(new Rectangle(300, 400, 50, 25), 0, 200);
+		Image i = new Image(new Rectangle(450, 150, 1280, 551), "src/imgs/cards.jpeg");
+		
+		i.crop(new Rectangle(0, 0, 99, 138));
+		//i.resize(0.6, false);
+		
+		w.addComponent(l);
+		w.addComponent(b);
+		w.addComponent(tb);
+		w.addComponent(nb);
+		w.addComponent(i);
+	 */
+	
+	private void initializeButtons() {
+		_buttons = new HashMap<String, Button>();
+		_buttons.put("go", new Button(new Rectangle(20, 310, 70, 30), "Go!", Color.YELLOW, Color.WHITE, null));
+
+		for (String button : _buttons.keySet()) {
+			addComponent(_buttons.get(button));
+		}
+	}
+	
+	private void initializeLabels() {
+		_lbl1 = new Label(new Point(20, 20), "Messages From Server", new TextStyle(Color.BLACK, "Arial", 12, false, false));
+		_messageFromServer = new Label(new Point(20, 50), "None", new TextStyle(Color.RED, "Arial", 12, false, false));
+
+		addComponent(_lbl1);
+		addComponent(_messageFromServer);
+	}
+	
+	private void initializeComponents() {
+		initializeLabels();
+		initializeButtons();
+	}
+}
