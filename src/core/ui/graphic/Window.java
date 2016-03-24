8adb607ac0b2864ef2866f46b387eda7701ded26
Matheus Tenorio <tenoriomatheus0@gmail.com>
2016-03-24 14:04:17 -0300

    Remake somethings in window class

--- src/core/ui/graphic/Window.java
+++ src/core/ui/graphic/Window.java
@@ -11,27 +11,31 @@ import core.ui.UI_Element;
 public class Window {
 	
 	private JFrame _frame;
-	private JPanel _panel;
+	private JPanel _defaltPanel;
 	
 	public Window(int width, int height, String name) {
 		_frame = new JFrame(name);
-		_panel = new JPanel(null);
+		_defaltPanel = new JPanel(null);
 		Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
 		
+		_defaltPanel.setLocation(0,0);
+		_defaltPanel.setSize(width, height);
+		
 		_frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		_frame.add(_panel);
+		_frame.add(_defaltPanel);
 		_frame.setSize(width, height);
 		_frame.setLocation((int)(screenSize.getWidth()/2 - _frame.getSize().width/2), (int)(screenSize.getHeight()/2 - _frame.getSize().height/2));
 		_frame.setVisible(true);
 		_frame.setResizable(false);
 	}
 	
-	public void addComponent(UI_Element element) {
-		_panel.add(element.getComponent());
-		_panel.repaint();
+	protected void addComponent(UI_Element element) {
+		_defaltPanel.add(element.getComponent());
+		_defaltPanel.repaint();
 	}
 	
-	public void removeComponent(UI_Element element) {
-		_panel.remove(element.getComponent());
+	protected void removeComponent(UI_Element element, String panel) {
+		_defaltPanel.remove(element.getComponent());
+		_defaltPanel.repaint();
 	}
 }
