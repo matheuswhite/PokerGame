93d90a98afdceb61293595db64a8946a04a33311
Matheus Tenorio <tenoriomatheus0@gmail.com>
2016-04-06 11:46:23 -0300

    Add playerInfo and Room

---
+++ src/core/domain/PlayerInfo.java
@@ -0,0 +1,57 @@
+package core.domain;
+
+import core.service.PrefixMultiplier;
+
+public class PlayerInfo {
+	
+	private long _id;
+	private Card[] _hand;
+	private Money _moneyPlayer;
+	private String _name;
+	private int _seat;
+	
+	public PlayerInfo(long id) {
+		_id = id;
+		_hand = new Card[2];
+		_moneyPlayer = new Money(0, PrefixMultiplier.NONE);
+		_name = "Player" + id;
+	}
+	
+	public Card[] getHand() {
+		return _hand;
+	}
+	public void setHand(Card[] hand) {
+		if (hand.length != 2)
+			throw new IllegalArgumentException("Number of cards invalid");
+		
+		_hand = hand;
+	}
+	
+	public long getId() {
+		return _id;
+	}
+	
+	public Money getMoneyPlayer() {
+		return _moneyPlayer;
+	}
+	public void removeMoney(Money money) {
+		_moneyPlayer.removeMoney(money);
+	}
+	public void addMoney(Money money) {
+		_moneyPlayer.addMoney(money);
+	}
+	
+	public String getName() {
+		return _name;
+	}
+	public void setName(String name) {
+		_name = name;
+	}
+	
+	public int getSeat() {
+		return _seat;
+	}
+	public void setSeat(int seat) {
+		_seat = seat;
+	}
+}
---
+++ src/core/domain/Room.java
@@ -0,0 +1,42 @@
+package core.domain;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import core.domain.MatchInfo;
+import core.domain.Money;
+import core.domain.PlayerInfo;
+
+public class Room {
+
+	private long _id;
+	private List<PlayerInfo> _listOfPlayers;
+	private MatchInfo _matchInfo;
+	public final static int ROOM_CAPACITY = 6;
+	
+	public Room(long id, Money smallBlindValue, Money minimumBuyIn) {
+		_id = id;
+		_matchInfo = new MatchInfo(smallBlindValue, minimumBuyIn);
+		_listOfPlayers = new ArrayList<PlayerInfo>(ROOM_CAPACITY);
+	}
+	
+	public long getId() {
+		return _id;
+	}
+	
+	public void addPlayer(PlayerInfo player) throws Exception {
+		if (_listOfPlayers.size() >= ROOM_CAPACITY)
+			throw new Exception("The max capacity of the room has been reached!");
+		_listOfPlayers.add(player);	
+	}
+	public void removePlayer(PlayerInfo player) {
+		_listOfPlayers.remove(player);
+	}
+	public List<PlayerInfo> getPlayers() {
+		return _listOfPlayers;
+	}
+	
+	public MatchInfo getMatchInfo() {
+		return _matchInfo;
+	}
+}
