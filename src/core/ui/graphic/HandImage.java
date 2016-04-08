e49bcbd84c5252075157d9834c1915a099dd329c
Matheus Tenorio <tenoriomatheus0@gmail.com>
2016-04-08 10:06:39 -0300

    Add hand image class

---
+++ src/core/ui/graphic/HandImage.java
@@ -0,0 +1,79 @@
+package core.ui.graphic;
+
+import java.awt.Point;
+import java.awt.Rectangle;
+
+import core.domain.Card;
+
+public class HandImage {
+
+	private Image _card1;
+	private Image _card2;
+	private Image _hiddenCards;
+	private boolean _hidden;
+	
+	public HandImage(Point location, Card card1, Card card2) {
+		_card1 = new Image(new Rectangle(location.x, location.y, 1280, 551), "src/imgs/cards.jpeg");
+		_card2 = new Image(new Rectangle(location.x + 25 /*testar*/, location.y, 1280, 551), "src/imgs/cards.jpeg");
+		_hiddenCards = new Image(new Rectangle(location.x, location.y, 90, 88), "src/imgs/hidenCards");
+		
+		_card1.crop(new Rectangle(99 * (card1.getNumber() - 1), 138 * (card1.getSuit().getValue()), 99, 137));
+		_card2.crop(new Rectangle(99 * (card2.getNumber() - 1), 138 * (card2.getSuit().getValue()), 99, 137));
+		
+		//testar
+		_card1.resize(0.65, false);
+		_card2.resize(0.65, false);
+		_hiddenCards.resize(1, false);
+		
+		_card1.hide();
+		_card2.hide();
+		_hiddenCards.show();
+		_hidden = true;
+	}
+	
+	public void flipCard() {
+		if (_hidden) {
+			_hiddenCards.hide();
+			_card1.show();
+			_card2.show();
+		}
+		else {
+			_card1.hide();
+			_card2.hide();
+			_hiddenCards.show();
+		}
+		_hidden = !_hidden;
+	}
+	
+	public void hide() {
+		if (_hidden) {
+			_hiddenCards.hide();
+		}
+		else {
+			_card1.hide();
+			_card2.hide();
+		}
+	}
+	
+	public void show() {
+		if (_hidden) {
+			_hiddenCards.show();
+		}
+		else {
+			_card1.show();
+			_card2.show();
+		}
+	}
+	
+	public Image getCard1() {
+		return _card1;
+	}
+	
+	public Image getCard2() {
+		return _card2;
+	}
+	
+	public Image getHiddenCards() {
+		return _hiddenCards;
+	}
+}
