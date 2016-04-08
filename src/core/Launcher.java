7dfc189e9ba6087eeb7e92ebf1c6c18d7bac388d
Matheus Tenorio <tenoriomatheus0@gmail.com>
2016-04-08 10:09:02 -0300

    Add match screen to test

--- src/core/Launcher.java
+++ src/core/Launcher.java
@@ -1,10 +1,12 @@
 package core;
 
 import core.ui.graphic.screen.ConnectionTestScreen;
+import core.ui.graphic.screen.MatchScreen;
 
 public class Launcher {
 	
 	public static void main(String[] args) {
-		ConnectionTestScreen screen = new ConnectionTestScreen();
+		//ConnectionTestScreen screen = new ConnectionTestScreen();
+		MatchScreen screen = new MatchScreen();
 	}
 }
