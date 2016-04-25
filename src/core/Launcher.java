b279e3441b6dba1e36140fb2784cd5e0886ada2f
DVDSON <dvdsoncomp@gmail.com>
2016-04-25 15:20:04 -0300

    Teste do Merge

--- src/core/Launcher.java
+++ src/core/Launcher.java
@@ -8,7 +8,7 @@ import core.ui.graphic.screen.MatchScreen;
 public class Launcher {
 	
 	public static void main(String[] args) {
-		PlayerInfo.Create(2314);
+		PlayerInfo.Create(2114);
 		//ConnectionTestScreen screen = new ConnectionTestScreen();
 		//MatchScreen screen = new MatchScreen(2049);
 		MainScreen screen = new MainScreen();
