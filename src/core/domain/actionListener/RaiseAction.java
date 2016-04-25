fd6b2146100af64513dfca4d9dc925d4b4d2bb68
DVDSON <dvdsoncomp@gmail.com>
2016-04-25 16:14:51 -0300

    Corrigindo RaiseAction

--- src/core/domain/actionListener/RaiseAction.java
+++ src/core/domain/actionListener/RaiseAction.java
@@ -5,19 +5,25 @@ import java.io.IOException;
 
 import core.net.Message;
 import core.net.ServerConnection;
+import core.domain.Money;
 
 public class RaiseAction extends ButtonAction {
+	private Money _money;
 	private ActionType _type;
 	
-	public RaiseAction() {
+	public RaiseAction(Money _money) {
 		super();
-
+		this._money = _money;
 		this._type = ActionType.RAISE;
+	}
+
+	public void raise(Money money){
+		_money.addMoney(money);
 	}
 	
 	public void actionPerformed(ActionEvent e) {
 		_content.clear();
-		
+		_content.add(_money);
 		_msg = new Message(1.0, _type.toString(), _content);
 		
 		try {
