dfa14681d63611f36133f59b0255041956996183
Matheus Tenorio <tenoriomatheus0@gmail.com>
2016-04-27 16:41:15 -0300

    Adicionando a classes para manipular arquivos JSON

--- src/core/storage/JSON_File.java
+++ src/core/storage/JSON_File.java
@@ -1,11 +1,21 @@
 package core.storage;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
 import com.google.gson.Gson;
 
 public class JSON_File implements Storage {
 
 	private static JSON_File _instance = null;
 	private Gson _gson;
+	private BufferedReader _reader;
+	private BufferedWriter _writer;
 	
 	private JSON_File() {
 		_gson = new Gson();
@@ -19,19 +29,39 @@ public class JSON_File implements Storage {
 	
 	@Override
 	public boolean exist(String filePath) {
-		return false;
+		File file = new File(filePath);
+		return file.exists();
 	}
 
 	@Override
 	public <T> T load(String filePath, Class<T> type) {
-		// TODO Auto-generated method stub
-		return null;
+		String jsonString = null;
+		
+		try {
+			_reader = new BufferedReader(new FileReader(filePath));
+			jsonString = _reader.readLine();
+			_reader.close();
+			
+		} catch (FileNotFoundException e1) {
+			e1.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		return _gson.fromJson(jsonString, type);
 	}
 
 	@Override
-	public void save(String file, Object object) {
-		// TODO Auto-generated method stub
-
+	public void save(String filePath, Object object) {
+		try {
+			_writer = new BufferedWriter(new FileWriter(filePath));
+			_writer.write(_gson.toJson(object));
+			_writer.close();
+			
+		} catch (FileNotFoundException e1) {
+			e1.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
-
 }
