1c490e3a070fe39121f0f166f3e69e5a5847b167
Matheus Tenorio <tenoriomatheus0@gmail.com>
2016-04-28 14:08:44 -0300

    Trocando o slider pelo money input

--- src/core/ui/graphic/EnterRoomPopUp.java
+++ src/core/ui/graphic/EnterRoomPopUp.java
@@ -5,13 +5,11 @@ import java.awt.Point;
 
 import javax.swing.JFrame;
 
-import core.domain.PlayerStats;
 import core.domain.Room;
-import core.service.Range;
 import core.ui.graphic.basics.Label;
 import core.ui.graphic.basics.PopUp;
-import core.ui.graphic.basics.Slider;
 import core.ui.graphic.basics.TextStyle;
+import core.ui.input.MoneyInput;
 
 public class EnterRoomPopUp extends PopUp {
 
@@ -21,26 +19,22 @@ public class EnterRoomPopUp extends PopUp {
 	private static final long serialVersionUID = 824213959521958735L;
 	
 	private Label _message;
-	private Slider _slider;
+	private MoneyInput _moneyInput;
 	
 	public EnterRoomPopUp(JFrame owner, String title) {
 		super(owner, title);
 		
 		_message = new Label(new Point(0, 0), "Enter with the Buy In value", new TextStyle(Color.BLACK, "Arial", 12, true, true));
-		_slider = new Slider(new Point(0, 0), new Range(0, 999), new TextStyle(Color.BLACK, "Arial", 12, true, false));
+		_moneyInput = new MoneyInput(new Point(0, 20));
 		
 		addContent(_message.getComponent());
-		addContent(_slider.getComponent());
+		addContent(_moneyInput.getComponent());
 		
-		setSize(350, 110);
+		setSize(260, 100);
 		//addActionToConfirmButton(action);
 	}
 	
 	public void setRoom(Room room) {
 		setTitle("Do you enter in the Room" + room.getId() + " ?");
-		
-		int min = room.getMatchInfo().getMinimumBuyIn().parseToInteger();
-		int max = PlayerStats.Instance().getMoney().parseToInteger();
-		_slider.setRange(new Range(min, max));
 	}
 }
