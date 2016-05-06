6d9be7de8766f9bccc5dea02c9972f3e5fc331fe
Matheus Tenorio <tenoriomatheus0@gmail.com>
2016-05-06 13:28:16 -0300

    Adicionando DisconnectHandler

---
+++ src/core/domain/messageHandler/DisconnectHandler.java
@@ -0,0 +1,13 @@
+package core.domain.messageHandler;
+
+import java.util.List;
+
+public class DisconnectHandler extends Handler {
+
+	@Override
+	public void handle(List<Object> content) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
