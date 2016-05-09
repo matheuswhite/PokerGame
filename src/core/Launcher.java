db09cd5c1e941bb3d0ac00118582a60ae8cf6922
Matheus Tenorio <tenoriomatheus0@gmail.com>
2016-05-09 14:17:07 -0300

    Commit test

--- src/core/Launcher.java
+++ src/core/Launcher.java
@@ -6,6 +6,6 @@ public class Launcher {
 	
 	public static void main(String[] args) {
 		@SuppressWarnings("unused")
-		MainScreen screen = new MainScreen();
+		MainScreen screen = new MainScreen();  
 	}
 }
