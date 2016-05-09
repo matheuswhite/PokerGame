bccfcede8fa7bfa94777605ab542271ed1d915ba
Matheus Tenorio <tenoriomatheus0@gmail.com>
2016-05-09 11:43:11 -0300

    Add ramo e uma classe de teste para a classe Money

---
+++ test/test/MoneyTest.java
@@ -0,0 +1,19 @@
+package test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class MoneyTest {
+
+	@Test
+	public void testAddMoney() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testRemoveMoney() {
+		fail("Not yet implemented");
+	}
+
+}
